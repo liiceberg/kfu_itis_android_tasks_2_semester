67b36254ef186486bc7619ddaae1861a65eb5313
liiceberg <2aysylu2003@gmail.com>
2024-03-05 10:55:12 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kfu_itis_android_tasks_2_semester"
+include(":app")
+ 
